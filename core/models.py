fd9bd552f64f35c7b6d73d40b780315271c6d1b8
mmj4594 <csmmj4594@postech.ac.kr>
2018-04-21 18:21:43 +0900

    model_ver3

--- core/models.py
+++ core/models.py
@@ -4,31 +4,33 @@ from django.utils import timezone
 #class User(models.Model):
 
 class Category(models.Model):
-	category = models.Charfield()
+	category = models.CharField(max_length=40)
 
 class Department(models.Model):
-	name = models.CharField()
+	name = models.CharField(max_length=40)
 	code = models.CharField(max_length=5)
 
 class Semester(models.Model):
-	name = models.CharField()#2018 1st semester
-	code = models.Charfield()#2018S
+	name = models.CharField(max_length=40)#2018 1st semester
+	code = models.CharField(max_length=40)#2018S
 
 class Subject(models.Model):
-	name = models.CharField()
-	code = models.CharField()
+	name = models.CharField(max_length=70)
+	code = models.CharField(max_length=40)
 	category = models.ForeignKey(Category)
 	department = models.ForeignKey(Department)
-	plan = models.textField()
-	professor = models.CharField()
-	class_number = models.integerField()
-	capacity = models.integerField()
-	credit = models.CharField()
+	plan = models.TextField()
+	professor = models.CharField(max_length=40)
+	class_number = models.IntegerField()
+	capacity = models.IntegerField()
+	credit = models.CharField(max_length=40)
 	semester = models.ForeignKey(Semester)
+	def to_dict(self):
+
 
 class Period(models.Model):
 	subject = models.ForeignKey(Subject)
-	place = models.CharField()
+	place = models.CharField(max_length=70)
 	start = models.TimeField()
 	end = models.TimeField()
 	mon = models.BooleanField()
@@ -36,12 +38,15 @@ class Period(models.Model):
 	wed = models.BooleanField()
 	thr = models.BooleanField()
 	fri = models.BooleanField()
+	def to_dict(self):
+
 
 class Alias(models.Model):
 	original = models.ForeignKey(Subject)
-	nickname = models.CharField()
+	nickname = models.CharField(max_length=70)
 
 class Timetable(models.Model):
 	user = models.ForeignKey(User) 
 	semester = models.ForeignKey(Semester)
 	subjects = models.ManyToManyField(Subject)
+	def to_dict(self):
