539e9b86fc4cf4991210c5c6176ac1465eee4356
Beomsu Lim <lbs1163@postech.ac.kr>
2018-04-21 16:46:38 +0900

    add skeleton code for url

---
+++ core/urls.py
@@ -0,0 +1,44 @@
+from django.conf.urls import urls
+from . import views
+
+urlpatterns = [
+	# 이건 내가 할꺼임
+	url(r'^$', views.timetable, name='timetable'),
+	# select semester
+	# get method로 요청이 들어오면 사용자 로그인 체크 (login_required decorator 검색해보셈)
+	# URL query: semester (학기 모델의 pk가 들어있음)
+	# example URL: select_semester/?semester=3
+	# user가 소유한 timetable 중에서 semester가 일치하는 것들을 JSON 형식으로 return
+	url(r'^select_semester/$', views.select_semester, name='select_semester'),
+	# add timetable
+	# post method로 요청이 들어오면 사용자 로그인 체크
+	# POST data: semester (학기 모델의 pk가 들어있음)
+	# 해당 semester로 user가 소유한 timetable을 새로 생성
+	# 생성 후 성공시 새로 생성된 timetable의 내용을 JSON 형식으로 return
+	url(r'^add_timetable/$', views.add_timetable, name='add_timetable'),
+	# delete timetable
+	# post method로 요청이 들어오면 사용자 로그인 체크
+	# POST data: timetable (시간표 모델의 pk가 들어있음)
+	# 해당 timetable이 user 소유인지 확인
+	# user 소유가 맞으면 timetable 삭제 후 성공 메시지를 JSON으로 return
+	# user 소유가 아니면 실패 메시지를 JSON으로 return
+	url(r'^delete_timetable/$', views.delete_timetable, name='delete_timetable'),
+	# copy timetable
+	# post method로 요청이 들어오면 사용자 로그인 체크
+	# POST data: timetable (시간표 모델의 pk가 들어있음)
+	# 해당 timetable이 user 소유인지 확인
+	# user 소유가 맞으면 새로운 timetable을 만듬
+	# 기존 timetable이 갖는 과목을 새로운 timetable도 갖게 만듬
+	# 성공하면 새로 생성된 timetable의 내용을 JSON 형식으로 return
+	# 실패하면 실패 메시지를 JSON으로 return
+	url(r'^copy_timetable/$', views.copy_timetable, name='copy_timetable'),
+	# search subject
+	# get method로 요청이 들어오면 사용자 로그인 체크
+	# URL query: q, hundreds, department, category, start_time, end_time, credit
+	# 검색 조건에 따라 해당 값이 쿼리에 없을 수도 있음
+	# ex) copy_timetable/?q=윤은영&hundreds=1
+	# 검색 조건에 맞는 subject들을 학수번호로 정렬하여 JSON으로 return
+	url(r'^search_subject/$', views.search_subject, name='search_subject'),
+	url(r'^add_subject_to_timetable/$', views.add_subject_to_timetable, name='add_subject_to_timetable'),
+	url(r'^delete_subject_to_timetable/$', views.delete_subject_to_timetable, name='delete_subject_to_timetable'),
+]
