587b8b27735a552a2a9b2209b3f7df54306e7019
anpandapoapper <jianjoo1212@naver.com>
2018-04-25 15:42:42 +0900

    add Http404

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 from django.shortcuts import render, get_object_or_404
 from django.contrib.sites.shortcuts import get_current_site
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.db.models import Q
 from django.views.generic import View
 from django.template.loader import render_to_string
