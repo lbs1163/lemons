ce43b697b50b6faf9165cb5b808e9b3df8aa2b2e
mmj4594 <csmmj4594@postech.ac.kr>
2018-05-21 20:34:33 +0900

    password_reset

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,15 @@ urlpatterns = [
 	url(r'^signup/$', Signup.as_view(), name='signup'),
 	url(r'^login/$', auth_views.LoginView.as_view(template_name='core/login.html', redirect_authenticated_user=True), name='login'),
 	url(r'^logout/$', auth_views.LogoutView.as_view(template_name='core/logout.html'), name='logout'),
+
+
+	url(r'^password_reset/$', auth_views.PasswordResetView.as_view(template_name='core/password_reset_form.html'), name="password_reset_form"),
+	url(r'^password_reset/done$', auth_views.PasswordResetDoneView.as_view(template_name='core/password_reset_done.html'), name="password_reset_done"),
+	url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+		auth_views.PasswordResetConfirmView.as_view(template_name='core/password_reset_confirm.html'), name="password_reset_confirm"),
+	url(r'^reset/done$', auth_views.PasswordResetCompleteView.as_view(template_name='core/password_reset_complete.html'), name="password_reset_complete"),
+
+
 	url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', Activate.as_view(), name='activate'),
 	url(r'^$', views.timetable, name='timetable'),
 	url(r'^test/$', views.test, name='test'),
